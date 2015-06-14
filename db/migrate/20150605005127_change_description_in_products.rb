61d860d83618d08dfb346b2afdc21b36a188084a
colleenmcguckin <colleenmcguckin@gmail.com>
2015-06-14 10:23:25 -0500

    trying to fix standard error heroku

--- db/migrate/20150605005127_change_description_in_products.rb
+++ db/migrate/20150605005127_change_description_in_products.rb
@@ -2,7 +2,7 @@ class ChangeDescriptionInProducts < ActiveRecord::Migration
   def change
     change_table :products do |t|
       t.change :description, :text
-      t.change :price, :decimal, precision: 8, scale:2
+      t.change :price, :string
       
       add_column :products, :in_stock, :boolean
     end
